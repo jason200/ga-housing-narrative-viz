dee2f00165dd853e8096959aede59fe8aa5d3908
jason200 <lovelynews79@gmail.com>
2025-07-24 23:42:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,20 +5,41 @@ const scenes = [drawScene1, drawScene2];
 // Main draw function
 function draw() {
   const svg = d3.select('#viz');
+  // clear previous marks
   svg.selectAll('*').remove();
+  // run current scene
   scenes[state.scene - 1]();
 }
 
 // Scene 1: simple bar chart
-def drawScene1() {
-  const data = [ {name: 'A', value: 30}, {name: 'B', value: 80}, {name: 'C', value: 45} ];
+function drawScene1() {
+  const data = [
+    { name: 'A', value: 30 },
+    { name: 'B', value: 80 },
+    { name: 'C', value: 45 }
+  ];
   const svg = d3.select('#viz');
-  const width = +svg.attr('width'), height = +svg.attr('height');
-  const x = d3.scaleBand().domain(data.map(d => d.name)).range([40, width-20]).padding(0.2);
-  const y = d3.scaleLinear().domain([0, 100]).range([height-30, 20]);
+  const width = +svg.attr('width'),
+        height = +svg.attr('height');
 
-  // axes\ n  svg.append('g').attr('transform', `translate(0,${height-30})`).call(d3.axisBottom(x));
-  svg.append('g').attr('transform', 'translate(40,0)').call(d3.axisLeft(y));
+  // scales
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.name))
+    .range([40, width - 20])
+    .padding(0.2);
+
+  const y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - 30, 20]);
+
+  // axes
+  svg.append('g')
+    .attr('transform', `translate(0,${height - 30})`)
+    .call(d3.axisBottom(x));
+
+  svg.append('g')
+    .attr('transform', 'translate(40,0)')
+    .call(d3.axisLeft(y));
 
   // bars
   svg.selectAll('.bar')
@@ -28,30 +49,54 @@ def drawScene1() {
       .attr('x', d => x(d.name))
       .attr('y', d => y(d.value))
       .attr('width', x.bandwidth())
-      .attr('height', d => height-30 - y(d.value))
+      .attr('height', d => height - 30 - y(d.value))
       .attr('fill', '#69b3a2');
 
   // annotation
   svg.append('text')
     .attr('class', 'annotation-note')
-    .attr('x', x('B') + x.bandwidth()/2)
+    .attr('x', x('B') + x.bandwidth() / 2)
     .attr('y', y(80) - 10)
     .attr('text-anchor', 'middle')
     .text('Highest value here');
 }
 
 // Scene 2: simple line chart
-def drawScene2() {
-  const data = [ {x:1,y:2}, {x:2,y:5}, {x:3,y:3}, {x:4,y:7} ];
+function drawScene2() {
+  const data = [
+    { x: 1, y: 2 },
+    { x: 2, y: 5 },
+    { x: 3, y: 3 },
+    { x: 4, y: 7 }
+  ];
   const svg = d3.select('#viz');
-  const width = +svg.attr('width'), height = +svg.attr('height');
-  const x = d3.scaleLinear().domain([1,4]).range([40, width-20]);
-  const y = d3.scaleLinear().domain([0,7]).range([height-30, 20]);
+  const width = +svg.attr('width'),
+        height = +svg.attr('height');
 
-  svg.append('g').attr('transform', `translate(0,${height-30})`).call(d3.axisBottom(x));
-  svg.append('g').attr('transform', 'translate(40,0)').call(d3.axisLeft(y));
+  // scales
+  const x = d3.scaleLinear()
+    .domain([1, 4])
+    .range([40, width - 20]);
 
-  const line = d3.line().x(d => x(d.x)).y(d => y(d.y));
+  const y = d3.scaleLinear()
+    .domain([0, 7])
+    .range([height - 30, 20]);
+
+  // axes
+  svg.append('g')
+    .attr('transform', `translate(0,${height - 30})`)
+    .call(d3.axisBottom(x));
+
+  svg.append('g')
+    .attr('transform', 'translate(40,0)')
+    .call(d3.axisLeft(y));
+
+  // line generator
+  const line = d3.line()
+    .x(d => x(d.x))
+    .y(d => y(d.y));
+
+  // draw line
   svg.append('path')
     .datum(data)
     .attr('fill', 'none')
@@ -59,6 +104,7 @@ def drawScene2() {
     .attr('stroke-width', 2)
     .attr('d', line);
 
+  // annotation
   svg.append('text')
     .attr('class', 'annotation-note')
     .attr('x', x(4))
@@ -67,10 +113,15 @@ def drawScene2() {
     .text('Peak value');
 }
 
-// Controls
-```html
-<script>
-  d3.select('#next').on('click', () => { state.scene = Math.min(state.scene+1, scenes.length); draw(); });
-  d3.select('#prev').on('click', () => { state.scene = Math.max(state.scene-1, 1); draw(); });
+// Wire up controls
+d3.select('#next').on('click', () => {
+  state.scene = Math.min(state.scene + 1, scenes.length);
   draw();
-</script>
+});
+d3.select('#prev').on('click', () => {
+  state.scene = Math.max(state.scene - 1, 1);
+  draw();
+});
+
+// Initial render
+draw();
